ec4b51082deada88e81c206f3bcca12d89482956
hyeonseog-won <smreo75@gmail.com>
2022-06-27 18:06:05 +0900

    중간 저장

---
+++ src/main/java/com/study/springbootwebservice/dto/HelloResponseDto.java
@@ -0,0 +1,10 @@
+package com.study.springbootwebservice.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter // 선언된 모든 필드의 get 메소드를 생성해줌
+@RequiredArgsConstructor    // 선언된 모든 final 필드가 포함된 생성자를 생성해줌.
+public class HelloResponseDto {
+
+}
