d7717d43f13fe11e90f0dd77b75c1115f08ee8ef
hyeonseog-won <smreo75@gmail.com>
2022-06-29 19:02:54 +0900

    프론트 posts 작성

--- src/main/java/com/study/springbootwebservice/controller/HelloController.java
+++ src/main/java/com/study/springbootwebservice/controller/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController //  컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 줌.
 public class HelloController {
-
     @GetMapping("/hello")   // HTTP Method의 GET 요청을 받을 수 있는 API를 만들어 줌.
     public String hello()  {
         return "hello";
